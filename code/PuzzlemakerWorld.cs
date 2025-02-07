b38212e92c77fc69687d5701b24466185da1ea6f
Igrium <thesam54123@gmail.com>
2025-02-06 17:23:29 -0800

    theory code for voxel system

---
+++ code/PuzzlemakerWorld.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Godot;
+
+namespace PuzzlemakerPlus;
+
+public class PuzzlemakerWorld : VoxelWorld<PuzzlemakerVoxel>
+{
+    
+}
+
+/// <summary>
+/// A single voxel in the puzzlemaker voxel world.
+/// While this class has a lot of helper functions, the struct itself is only one byte.
+/// </summary>
+public struct PuzzlemakerVoxel
+{
+    [Flags]
+    public enum VoxelFlags
+    {
+        Open = 0,
+        XP = 1,
+        XN = 2,
+        YP = 4,
+        YN = 8,
+        ZP = 16,
+        ZN = 32
+    }
+
+    public byte Flags;
+
+    public bool IsOpen => HasFlag(VoxelFlags.Open);
+    public bool XPositive => HasFlag(VoxelFlags.XP);
+    public bool XNegative => HasFlag(VoxelFlags.XN);
+    public bool YPositive => HasFlag(VoxelFlags.YP);
+    public bool YNegative => HasFlag(VoxelFlags.YN);
+    public bool ZPositive => HasFlag(VoxelFlags.ZP);
+    public bool ZNegative => HasFlag(VoxelFlags.ZN);
+
+    public PuzzlemakerVoxel SetXPositive(bool portalable)
+    {
+        return portalable ? SetFlag(VoxelFlags.XP) : ClearFlag(VoxelFlags.XP);
+    }
+
+    public PuzzlemakerVoxel SetXNegative(bool portalable)
+    {
+        return portalable ? SetFlag(VoxelFlags.XN) : ClearFlag(VoxelFlags.XN);
+    }
+
+    public PuzzlemakerVoxel SetYPositive(bool portalable)
+    {
+        return portalable ? SetFlag(VoxelFlags.YP) : ClearFlag(VoxelFlags.YP);
+    }
+
+    public PuzzlemakerVoxel SetYNegative(bool portalable)
+    {
+        return portalable ? SetFlag(VoxelFlags.YN) : ClearFlag(VoxelFlags.YN);
+    }
+
+    public PuzzlemakerVoxel SetZPositive(bool portalable)
+    {
+        return portalable ? SetFlag(VoxelFlags.ZP) : ClearFlag(VoxelFlags.ZP);
+    }
+
+    public PuzzlemakerVoxel SetZNegative(bool portalable)
+    {
+        return portalable ? SetFlag(VoxelFlags.ZN) : ClearFlag(VoxelFlags.ZN);
+    }
+
+    public bool HasFlag(VoxelFlags flag)
+    {
+        return ((Flags & (byte)flag) == (byte)flag);
+    }
+
+    public PuzzlemakerVoxel SetFlag(VoxelFlags flag)
+    {
+        PuzzlemakerVoxel result = this;
+        result.Flags |= (byte)flag;
+        return result;
+    }
+
+    public PuzzlemakerVoxel SetFlags(params VoxelFlags[] flags)
+    {
+        PuzzlemakerVoxel result = this;
+        foreach (VoxelFlags flag in flags)
+        {
+            result.Flags |= (byte)flag;
+        }
+        return result;
+    }
+
+    public PuzzlemakerVoxel ClearFlag(VoxelFlags flag)
+    {
+        PuzzlemakerVoxel result = this;
+        Flags &= (byte)~flag;
+        return result;
+    }
+
+    public PuzzlemakerVoxel ClearFlags(params VoxelFlags[] flags)
+    {
+        PuzzlemakerVoxel result = this;
+        foreach (VoxelFlags flag in flags)
+        {
+            result.Flags &= (byte)~flag;
+        }
+        return result;
+    }
+}
--- code/VoxelWorld.cs
+++ code/VoxelWorld.cs
@@ -1,6 +1,107 @@
 using Godot;
-using System;
+using System.Collections.Generic;
 
-public partial class VoxelWorld : Node
+namespace PuzzlemakerPlus;
+public class VoxelWorld<T>
 {
+    private readonly Dictionary<Vector3I, VoxelChunk<T>> chunks = new();
+
+    /// <summary>
+    /// A dictionary of all the chunks in this world.
+    /// </summary>
+    public IDictionary<Vector3I, VoxelChunk<T>> Chunks => chunks;
+
+    public T? Get(int x, int y, int z)
+    {
+        int chunkX = x >> 4;
+        int chunkY = y >> 4;
+        int chunkZ = z >> 4;
+
+        int localX = x & 15;
+        int localY = y & 15;
+        int localZ = z & 15;
+
+        var chunk = chunks.GetValueOrDefault(new Vector3I(localX, localY, localZ));
+        if (chunk == null) return default;
+
+        return chunk.Get(localX, localY, localZ);
+    }
+
+    public T? Get(Vector3I pos)
+    {
+        return Get(pos.X, pos.Y, pos.Z);
+    }
+
+    public T? Set(int x, int y, int z, T value)
+    {
+        int chunkX = x >> 4;
+        int chunkY = y >> 4;
+        int chunkZ = z >> 4;
+
+        int localX = x & 15;
+        int localY = y & 15;
+        int localZ = z & 15;
+
+        Vector3I chunkPos = new Vector3I(localX, localY, localZ);
+        var chunk = chunks.GetValueOrDefault(chunkPos);
+
+        if (chunk == null)
+        {
+            if (EqualityComparer<T>.Default.Equals(value, default))
+                return default;
+
+            chunk = new VoxelChunk<T>();
+            chunks[chunkPos] = chunk;
+        }
+
+        return chunk.Set(localX, localY, localZ, value);
+    }
+
+    public T? Set(Vector3I pos, T value)
+    {
+        return Set(pos.X, pos.Y, pos.Z, value);
+    }
+}
+
+/// <summary>
+/// A 16x16 array of blocks.
+/// </summary>
+/// <typeparam name="T">Block type</typeparam>
+public class VoxelChunk<T>
+{
+    private readonly T[] data = new T[16 * 16 * 16];
+
+    /// <summary>
+    /// The flat array containing the chunk's data.
+    /// </summary>
+    public T[] Data { get => data; }
+
+    /// <summary>
+    /// Get the value at a specific locaiton.
+    /// </summary>
+    /// <param name="x">Local X coordinate.</param>
+    /// <param name="y">Local Y coordinate.</param>
+    /// <param name="z">Local Z coordinate.</param>
+    /// <returns>The value.</returns>
+    public T Get(int x, int y, int z)
+    {
+        int index = x + (y * 16) + (z * 16 * 16);
+        return data[index];
+    }
+
+    /// <summary>
+    /// Set the value at a specific location.
+    /// </summary>
+    /// <param name="x">Local X coordinate.</param>
+    /// <param name="y">Local Y coordinate.</param>
+    /// <param name="z">Local Z coordinate.</param>
+    /// <param name="value">New value.</param>
+    /// <returns>The previous value.</returns>
+    public T Set(int x, int y, int z, T value)
+    {
+        int index = x + (y * 16) + (z * 16 * 16);
+        T prev = data[index];
+        data[index] = value;
+        return prev;
+    }
 }
