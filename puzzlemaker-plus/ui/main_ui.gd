0ea86ad6036f70434ea89e10ea0d459cc1c55dc3
Igrium <thesam54123@gmail.com>
2025-03-13 14:30:40 -0700

    re-implemented export gltf debug option

--- puzzlemaker-plus/ui/main_ui.gd
+++ puzzlemaker-plus/ui/main_ui.gd
@@ -48,3 +48,11 @@ func _on_redo_pressed() -> void:
 
 func _on_add_test_item_pressed() -> void:
 	Editor.AddItem("testitem", Vector3(0, 0, 6))
+
+func _on_export_gltf_pressed() -> void:
+	var gltf_document_save := GLTFDocument.new()
+	var gltf_state_save := GLTFState.new()
+	gltf_document_save.append_from_scene(get_tree().root, gltf_state_save)
+
+	gltf_document_save.write_to_filesystem(gltf_state_save, "user://scene.gltf")
+	print("Wrote gltf to " + ProjectSettings.globalize_path("user://scene.gltf"))
